1039da42c253a5ea93b38cdc9f899bb0a90d90b5
mariiiiams <linalia.mishchenko75@gmail.com>
2025-11-14 11:11:32 +0100

    Commit 3: Add method calling to the test_input_validator.py

--- test_input_validator.py
+++ test_input_validator.py
@@ -1,7 +1,13 @@
 import Input_Validator
 
 user_input_1 = ["0", "1", "-2", "3.14", "four", "5", "6"]
-user_input_2 = ["-1", "-5", "hello", "0", "3.5", "8", "10"]
+user_input_2 = ["-1", "-5", "hello", "0", "3.5", "8.0", "-10"]
 
 validator1 = Input_Validator.UserInputValidator()
 validator2 = Input_Validator.UserInputValidator()
+
+input1_validation = validator1.validate_positive_integer(user_input_1)
+input2_validation = validator2.validate_positive_integer(user_input_2)
+
+result1 = print(validator1.msg_validation())
+result2 = print(validator2.msg_validation())    
