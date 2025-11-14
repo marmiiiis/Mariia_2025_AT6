459f3d4819cfce03d53d3d58c1578ea315e94c43
mariiiiams <linalia.mishchenko75@gmail.com>
2025-11-14 10:57:34 +0100

    Commit 2: Add test_input_validator.py and .gitignore

---
+++ test_input_validator.py
@@ -0,0 +1,7 @@
+import Input_Validator
+
+user_input_1 = ["0", "1", "-2", "3.14", "four", "5", "6"]
+user_input_2 = ["-1", "-5", "hello", "0", "3.5", "8", "10"]
+
+validator1 = Input_Validator.UserInputValidator()
+validator2 = Input_Validator.UserInputValidator()
